c2a4889efeb39562d2e4473cd723327b934b8d3b
amilaerandika <amilaerandikasampth@gmail.com>
2021-09-13 14:20:33 +0530

    Amila Added Report - Debit Note to view

--- JEDB/ReportViewer.cs
+++ JEDB/ReportViewer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,10 @@ namespace JEDB
         public frmReportViewer()
         {
             InitializeComponent();
+            ReportDocument cryRpt = new ReportDocument();
+            cryRpt.Load(@"C:\Users\amila\Desktop\JEDB\JEDB\Reports\DebitNote.rpt");
+            crystalReportViewerJEDB.ReportSource = cryRpt;
+            crystalReportViewerJEDB.Refresh();
         }
     }
 }
